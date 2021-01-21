131623c8519b3b1f62346d69395b135b370f3691
bahija siddiqui <bahijasiddiqui006@gmail.comgit config --global user.name 4bahijagit config --global user.email bahijasiddiqui006@gmail.com>
2021-01-21 21:26:31 +0530

    printing board

--- game.py
+++ game.py
@@ -59,5 +59,16 @@ def two_player():
                 ---|---|---
                 7 | 8 | 9
                 """)
-
+    def print_board():
+        print("   |   |   ")
+        print(" "+board[1]+" | "+board[2]+" | "+board[3]+" ")
+        print("   |   |   ")
+        print("---|---|---")
+        print("   |   |   ")
+        print(" "+board[4]+" | "+board[5]+" | "+board[6]+" ")
+        print("   |   |   ")
+        print("---|---|---")
+        print("   |   |   ")
+        print(" "+board[7]+" | "+board[8]+" | "+board[9]+" ")
+        print("   |   |   ")
 

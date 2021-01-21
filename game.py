dea38b6f2a94d64c32f6bbe04b6456efc7f4f81d
bahija siddiqui <bahijasiddiqui006@gmail.comgit config --global user.name 4bahijagit config --global user.email bahijasiddiqui006@gmail.com>
2021-01-21 21:16:48 +0530

    defining welcome function

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
 import os
 import time
 import random
+
+def welcome():
+    for i in range(0, 11):
+        print("*", end="")
+    print()
+    print("* WELCOME *", end="")
+    print()
+    for i in range(0, 11):
+        print("*", end="")
+    print()
+    input("Press ENTER key to start")
+    option()
+    

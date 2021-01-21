b2fbd31d0c3f5013a0b7e6c317403300287f6e94
bahija siddiqui <bahijasiddiqui006@gmail.comgit config --global user.name 4bahijagit config --global user.email bahijasiddiqui006@gmail.com>
2021-01-21 20:14:00 +0530

    added images

--- game.py
+++ game.py
@@ -3,44 +3,44 @@ from pygame.locals import *
 import time
 
 
-XO='x'
+XO = 'x'
 winner = None
-draw= False
-width= 400
-height= 400
-white= (255,255,255)
-line_color= (10,10,10)
+draw = False
+width = 400
+height = 400
+white = (255, 255, 255)
+line_color = (10, 10, 10)
 
-TTT = [[None]*3,[None]*3,[None]*3]
+TTT = [[None]*3, [None]*3, [None]*3]
 
 pg.init()
-fps= 30
-CLOCK=pg.time.Clock()
-screen=pg.display.set_mode((width,height+100),0,32)
+fps = 30
+CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width, height+100), 0, 32)
 pg.display.set_caption("Tic Tac Toe")
 
 
 opening = pg.image.load('tto.png')
-x_img=pg.image.load('x.png')
-o_img= pg.image.load('o.png')
+x_img = pg.image.load('x.png')
+o_img = pg.image.load('o.png')
 
-x_img=pg.transform.scale(x_img,(80,80))
-o_img=pg.transform.scale(o_img,(80,80))
-opening=pg.transform.scale(opening,(width,height+100))
+x_img = pg.transform.scale(x_img, (80, 80))
+o_img = pg.transform.scale(o_img, (80, 80))
+opening = pg.transform.scale(opening, (width, height+100))
 
 
 def game_opening():
-    screen.blit(opening,(0,0))
+    screen.blit(opening, (0, 0))
     pg.display.update()
     time.sleep(1)
     screen.fill(white)
 
 
-    pg.draw.line(screen,line_color,(width/3,0),(width/3,height),7)
-    pg.draw.line(screen,line_color,(width/3*2,0),(width/3*2,height),7)
+    pg.draw.line(screen, line_color, (width/3, 0), (width/3, height), 7)
+    pg.draw.line(screen, line_color, (width/3*2, 0), (width/3*2, height), 7)
 
-    pg.draw.line(screen,line_color,(0,height/3),(width,height/3),7)
-    pg.draw.line(screen,line_color,(0,height/3*2),(width,height/3*2),7)
+    pg.draw.line(screen, line_color, (0, height/3), (width, height/3), 7)
+    pg.draw.line(screen, line_color, (0, height/3*2), (width, height/3*2), 7)
 
     draw_status()
 
@@ -49,19 +49,19 @@ def draw_status():
     global draw
 
     if winner is None:
-        message= XO.upper() + "'s Turn"
+        message = XO.upper() + "'s Turn"
     else:
-        message=winner.upper() + "won"
+        message = winner.upper() + "won"
 
     if draw:
-        message=" The Game is a Draw"
+        message = " The Game is a Draw"
 
-    font=pg.font.Font(None,30)
-    text=font.render(message,1,(255,255,255))
+    font = pg.font.Font(None, 30)
+    text = font.render(message, 1, (255, 255, 255))
 
-    screen.fill((0,0,0),(0,400,500,100))
-    text_rect =text.get_rect(center=(width/2,500-50))
-    screen(text,text_rect)
+    screen.fill((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width/2, 500-50))
+    screen(text, text_rect)
     pg.display.update()
 
 
